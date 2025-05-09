89b1239edc48df01110bf59accf1cbf7d5c68000
Michael Lackey <michael.g.lackey@gmail.com>
2025-05-09 18:03:59 +0300

    full CRUD on auth, user, and recipe

---
+++ routes/recipeRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const recipeController = require('../controllers/recipeController.js')
+
+router.post('/', recipeController.createRecipe)
+router.get('/', recipeController.getAllRecipes)
+router.get('/:id', recipeController.getRecipeById)
+router.put('/:id', recipeController.updateRecipeById)
+router.delete('/:id', recipeController.deleteRecipeById)
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const logger = require('morgan')
+const methodOverride = require('method-override')
+const session = require('express-session')
+require('dotenv').config()
+
+const authRouter = require('./routes/authRouter.js')
+const userRouter = require('./routes/userRouter.js')
+const recipeRouter = require('./routes/recipeRouter.js')
+
+const PORT = process.env.PORT ? process.env.PORT : 3000
+
+const db = require('./db')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
+app.use('/auth', authRouter)
+app.use('/users', userRouter)
+app.use('/recipes', recipeRouter)
+
+app.use('/', (req, res) => {
+  res.send('Our app is connected . . . ')
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Server on Port ${PORT} . . . `)
+})
